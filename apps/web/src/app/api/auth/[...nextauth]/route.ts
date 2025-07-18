0bf9e241966f55524a8b41b615361d66548df00c
max-mackie <maxmackie13@gmail.com>
2025-07-18 22:01:22 +0100

    fix(config): configure ts in db package

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,12 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   adapter: DrizzleAdapter(db),
   providers: [Google],
 });
