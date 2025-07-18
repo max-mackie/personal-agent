994fb4cb6f67ba97af45777dc083169157ceab08
max-mackie <maxmackie13@gmail.com>
2025-07-18 22:08:09 +0100

    fix(auth)

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,2 @@
-import { db } from "@personal-agent/db";
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  adapter: DrizzleAdapter(db),
-  providers: [Google],
-});
+import { handlers } from "@/auth";
+export const { GET, POST } = handlers;
